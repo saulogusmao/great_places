3a99831e54372bb591f9b50d7657dcc6916da6f0
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2022-11-16 16:34:44 -0300

    Show Map

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:great_places/models/place.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
+  final PlaceLocation initialLocation;
+  const MapScreen(
+      {this.initialLocation = const PlaceLocation(
+        latitude: 37.422131,
+        longitude: -122.084801,
+        address: (''),
+      )});
 
   @override
   State<MapScreen> createState() => _MapScreenState();
@@ -11,6 +20,19 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Selecione..'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.initialLocation.latitude,
+            widget.initialLocation.longitude,
+          ),
+          zoom: 13,
+        ),
+      ),
+    );
   }
 }
