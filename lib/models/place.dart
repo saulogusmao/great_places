63cdf2fe5edabbefea7621d9fac38571dd4eb080
Saulo Gusmao <saulogusmaoribeiro@hotmail.com>
2022-11-20 20:58:03 -0300

    PlaceDetail Screen

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,16 +1,20 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PlaceLocation {
   final double latitude;
   final double longitude;
-  final String address;
+  final String address; // para remover o 'required', adicionar '?' após String
 
   const PlaceLocation({
     required this.latitude,
     required this.longitude,
     required this.address,
   });
+  LatLng toLatLng() {
+    return LatLng(latitude, longitude);
+  }
 }
 
 class Place {
--- lib/screens/place_detail_screen.dart
+++ lib/screens/place_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:great_places/models/place.dart';
+import 'package:great_places/screens/map_screen.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
   const PlaceDetailScreen({super.key});
@@ -12,6 +13,44 @@ class PlaceDetailScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(place.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 250,
+            width: double.infinity,
+            child: Image.file(
+              place.image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            place.location!.address,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey,
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextButton.icon(
+            icon: const Icon(Icons.map),
+            label: const Text('Ver no Mapa'),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (ctx) => MapScreen(
+                    isReadOnly: true,
+                    initialLocation: place.location!,
+                  ),
+                ),
+              );
+            },
+          )
+        ],
       ),
     );
   }
